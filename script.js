7d67d9aece194b200fcbc0d751e31a860dbd86d4
André Vennberg <mac@macs-MacBook-Air.local>
2015-09-04 14:51:51 +0200

    stuff

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-var func = function () {
-    document.getElementById("log").innerHTML += "Test<br>";
+var func = function() {
+    for(i = 0; i < 201; i += 2) {
+        log(i);
+    }
+
+    for(i = 7; i < 501; i += 7) {
+        log(i);
+    }
+
+    for(i = 3; i <= 30; i += 3) {
+        log(i);
+    }
+
+    for(i = 2; i < 11; i++) {
+        for(j = i; j <= (i * 10); j += i) {
+            log(j);
+        }
+    }
 }
 
-var clearDiv = function () {
+var log = function(i) {
+    document.getElementById("log").innerHTML += i + "<br>";
+}
+
+var clearDiv = function() {
     document.getElementById("log").innerHTML = "";
 }
