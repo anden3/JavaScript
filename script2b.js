bf754c7d454cf66a08720f3ea6a51a867a74cd0a
André Vennberg <mac@macs-MacBook-Air.local>
2015-09-08 09:54:39 +0200

    improved stick figure

--- script2b.js
+++ script2b.js
@@ -2,11 +2,41 @@ $(document).ready(function() {
     var canv = document.getElementById("c");
     var ctx = canv.getContext("2d");
 
+    ctx.save();
+
     ctx.beginPath();
+
     ctx.arc(420, 70, 40, 0, 2 * Math.PI);
 
+    ctx.scale(1, 2);
+    ctx.arc(420, 110, 40, 0, 2 * Math.PI);
+
+    ctx.fillStyle = '#8ED6FF';
+    ctx.fill();
+
+    ctx.closePath();
+
+    ctx.restore();
+
+    ctx.beginPath();
+
     ctx.moveTo(420, 110);
-    ctx.lineTo(420, 160);
+    ctx.lineTo(420, 140);
+
+    ctx.moveTo(455, 180);
+    ctx.lineTo(480, 240);
+
+    ctx.moveTo(385, 180);
+    ctx.lineTo(360, 240);
+
+    ctx.moveTo(440, 289);
+    ctx.lineTo(450, 400);
+
+    ctx.moveTo(400, 289);
+    ctx.lineTo(390, 400);
+
+    ctx.closePath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'black';
     ctx.stroke();
 });
-
