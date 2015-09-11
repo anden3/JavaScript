89c62a6360cfca88116054038ca7e511b623a1ba
André Vennberg <mac@macs-MacBook-Air.local>
2015-09-11 16:44:58 +0200

    added paddle with working collision detection, phew...

--- Pong/pong.js
+++ Pong/pong.js
@@ -1,12 +1,14 @@
 var canvas = document.getElementById("canv");
 var ctx = canvas.getContext("2d");
 
-var ballX = 300;
+var ballX = 300, ballY = 350;
+var ballVX = 2, ballVY = 1;
+
+var p1Y = 300, p1VY = 0;
 
 var paintBall = function() {
-    ctx.clearRect(0, 0, 800, 600);
     ctx.beginPath();
-    ctx.arc(ballX, 300, 5, 0, 2 * Math.PI);
+    ctx.arc(ballX, ballY, 5, 0, 2 * Math.PI);
     ctx.fillStyle = "rgba(255, 0, 150, 0.9)";
     ctx.fill();
     ctx.lineWidth = 3;
@@ -14,9 +16,57 @@ var paintBall = function() {
     ctx.stroke();
     ctx.closePath();
 
-    ballX += 1;
+    ballX += ballVX;
+    ballY += ballVY;
+
+    //Höger- och vänsterkant
+    if(ballX >= 795 || ballX <= 5) {
+        ballVX *= -1;
+    }
+    //Botten- och toppkant
+    if(ballY >= 595 || ballY <= 5) {
+        ballVY *= -1;
+    }
+
+    if(Math.abs(ballX - 20) === 10 && Math.abs(ballY - (p1Y + 50) <= 50)) {
+        ballVX *= -1;
+    }
 }
 
-paintBall();
+var paintRect = function() {
+    ctx.fillRect(15, p1Y, 10, 100);
+    p1Y += p1VY;
+}
 
-window.setInterval(paintBall, 1000 / 60);
+var update = function() {
+    ctx.clearRect(0, 0, 800, 600);
+    paintRect();
+    paintBall();
+}
+
+var keyDown = function(e) {
+    if(e.keyCode === 87 || e.keyCode === 38) {
+        if(p1Y <= 0) {
+            p1VY = 0;
+        }
+        else {
+            p1VY = -2;
+        }
+    }
+    if(e.keyCode === 83 || e.keyCode === 40) {
+        if(p1Y >= 500) {
+            p1VY = 0;
+        }
+        else {
+            p1VY = 2;
+        }
+    }
+}
+
+var keyUp = function(e) {
+    if(e.keyCode === 87 || e.keyCode === 83 || e.keyCode === 38 || e.keyCode === 40) {
+        p1VY = 0;
+    }
+}
+
+window.setInterval(update, 1000 / 60);
