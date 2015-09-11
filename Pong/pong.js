d0934d7ee682257d472ee34362aca5e9b56068c1
André Vennberg <mac@macs-MacBook-Air.local>
2015-09-11 13:57:37 +0200

    continued development of Pong

--- Pong/pong.js
+++ Pong/pong.js
@@ -1,5 +1,22 @@
 var canvas = document.getElementById("canv");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(400, 300, 5, 0, 2 * Math.PI);
+var ballX = 300;
+
+var paintBall = function() {
+    ctx.clearRect(0, 0, 800, 600);
+    ctx.beginPath();
+    ctx.arc(ballX, 300, 5, 0, 2 * Math.PI);
+    ctx.fillStyle = "rgba(255, 0, 150, 0.9)";
+    ctx.fill();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#B2B200";
+    ctx.stroke();
+    ctx.closePath();
+
+    ballX += 1;
+}
+
+paintBall();
+
+window.setInterval(paintBall, 1000 / 60);
