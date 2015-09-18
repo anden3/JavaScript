a4ab854780fe64575ae235a11676b7effe564dfc
André Vennberg <mac@macs-MacBook-Air.local>
2015-09-18 14:53:40 +0200

    added total scalability

--- Pong/pong.js
+++ Pong/pong.js
@@ -2,12 +2,25 @@
 var canvas = document.getElementById("canv");
 var ctx = canvas.getContext("2d");
 
+//Setting the canvas dimensions to the maximum avaliable size within the window
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
+//Adding event handlers for the keys and mouse
+document.getElementById("main").addEventListener("keydown", function(){ keyDown(event); });
+document.getElementById("main").addEventListener("keyup", function(){ keyUp(event); });
+document.getElementById("main").addEventListener("mousemove", function(){ mouseMove(event); });
+window.addEventListener("resize", function(){ updateDimensions(); });
+
+//Setting the height and width of the canvas to variables
+var dimX = ctx.canvas.height, dimY = ctx.canvas.width;
+
 //Defining variables for position and speed of ball
 var ballX = 0, ballY = 0;
 var ballVX = 0, ballVY = 0;
 
 //Y-values and speed for paddles
-var p1Y = canvas.height / 2 - 50, p1VY = 0;
+var p1Y = dimX/2 - dimX/16, p1VY = 0;
 var p2Y = 0;
 
 //Initial score
@@ -21,7 +34,7 @@ var soundMiss = new Audio("http://cs.au.dk/~dsound/DigitalAudio.dir/Greenfoot/Po
 var paintBall = function() {
     //Drawing the ball
     ctx.beginPath();
-        ctx.arc(ballX, ballY, 5, 0, 2 * Math.PI);
+        ctx.arc(ballX, ballY, dimX/160, 0, 2 * Math.PI);
         ctx.fillStyle = "#FFFFFF";
         ctx.fill();
     ctx.closePath();
@@ -31,29 +44,29 @@ var paintBall = function() {
     ballY += ballVY;
 
     //Right edge col-detection
-    if(ballX >= canvas.width - 5) {
+    if(ballX >= dimY - dimY/160) {
         p1S += 1;
         soundMiss.play();
         resetBall();
     }
     //Left edge col-detection
-    if(ballX <= 5) {
+    if(ballX <= dimY/288) {
         p2S += 1;
         soundMiss.play();
         resetBall();
     }
     //Bottom and top edge col-detection
-    if(ballY >= canvas.height - 5 || ballY <= 5) {
+    if(ballY >= dimX - dimX/160 || ballY <= dimX/160) {
         soundWall.play();
         ballVY *= -1;
     }
     //Paddle 1 col-detection
-    if(Math.abs(ballX - 20) <= 10 && Math.abs(ballY - (p1Y + 50)) <= 50) {
+    if(Math.abs(ballX - dimY/72) <= dimY/144 && Math.abs(ballY - (p1Y + dimX/16)) <= dimX/16) {
         soundPaddle.play();
         ballVX *= -1;
     }
     //Paddle 2 col-detection
-    if(Math.abs(ballX - (canvas.width - 20)) <= 10 && Math.abs(ballY - (p2Y + 50)) <= 50) {
+    if(Math.abs(ballX - (dimY - dimY/72)) <= dimY/144 && Math.abs(ballY - (p2Y + dimX/16)) <= dimX/16) {
         soundPaddle.play();
         ballVX *= -1;
     }
@@ -61,8 +74,8 @@ var paintBall = function() {
 
 var paintRect = function() {
     //Drawing the paddles
-    ctx.fillRect(15, p1Y, 10, 100);
-    ctx.fillRect(canvas.width - 25, p2Y, 10, 100);
+    ctx.fillRect(dimY/96, p1Y, dimY/144, dimX/8);
+    ctx.fillRect(dimY - (dimY/57.6), p2Y, dimY/144, dimX/8);
 
     //Moving the left paddle
     p1Y += p1VY;
@@ -71,26 +84,27 @@ var paintRect = function() {
     ctx.beginPath();
         //Setting the style of the line
         ctx.strokeStyle = "#FFFFFF";
-        ctx.lineWidth = 20;
-        ctx.setLineDash([21]);
+        ctx.lineWidth = dimX/40;
+        ctx.setLineDash([dimX/40]);
 
         //Drawing the line
-        ctx.moveTo(canvas.width / 2, -3);
-        ctx.lineTo(canvas.width / 2, canvas.height);
+        ctx.moveTo(dimY/2, -3);
+        ctx.lineTo(dimY/2, dimX);
         ctx.stroke();
     ctx.closePath();
 }
 
 var paintText = function() {
     //Drawing the score displays
-    ctx.font = "100px monospace";
-    ctx.fillText(p1S, canvas.width / 2 - 100, canvas.height / 6);
-    ctx.fillText(p2S, canvas.width / 2 + 40, canvas.height / 6);
+    ctx.font = dimX/8 + "px monospace";
+    var textLength = ctx.measureText(p1S).width;
+    ctx.fillText(p1S, dimY/2 - (textLength + dimY/36), dimX/6);
+    ctx.fillText(p2S, dimY/2 + dimY/36, dimX/6);
 }
 
 var update = function() {
     //Clearing the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, dimY, dimX);
 
     //Calling all drawing functions
     paintRect();
@@ -98,17 +112,27 @@ var update = function() {
     paintText();
 }
 
+var updateDimensions = function() {
+    //Setting the canvas dimensions to the maximum avaliable size within the window
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+
+    //Setting the height and width of the canvas to variables
+    dimX = ctx.canvas.height;
+    dimY = ctx.canvas.width;
+}
+
 var resetBall = function() {
     //Setting ball spawn location to the center of the canvas
-    ballX = canvas.width / 2 - 5;
-    ballY = canvas.height / 2 - 5;
+    ballX = dimY/2 - dimY/288;
+    ballY = dimX/2 - dimX/160;
 
     //Saving a value of either -1 or 1 to a variable
     var posOrNeg = Math.random() < 0.5 ? -1 : 1;
 
     //Setting ball speeds to random values between 1 and 5, and multiplies it by either 1 or -1
-    ballVX = posOrNeg * (Math.random() * (5 - 1 + 1) + 1);
-    ballVY = posOrNeg * (Math.random() * (5 - 1 + 1) + 1);
+    ballVX = posOrNeg * (Math.random() * ((dimY/288) - (dimY/1440)) + (dimY/1440));
+    ballVY = posOrNeg * (Math.random() * ((dimX/160) - (dimX/800)) + dimX/800);
     update();
 }
 
@@ -121,18 +145,18 @@ var keyDown = function(e) {
         }
         else {
             //Moves the paddle upwards
-            p1VY = -5;
+            p1VY = -(dimX/160);
         }
     }
     //Checking if 'S' or 'DownArrow' is being pressed
     if(e.keyCode === 83 || e.keyCode === 40) {
         //Only moves the paddle if it's not at the bottom already
-        if(p1Y >= canvas.height - 100) {
+        if(p1Y >= dimX - dimX/8) {
             p1VY = 0;
         }
         else {
             //Moves the paddle downwards
-            p1VY = 5;
+            p1VY = dimX/160;
         }
     }
 }
@@ -145,12 +169,12 @@ var keyUp = function(e) {
 }
 
 var mouseMove = function(e) {
-    //Saves the mouses Y-coordinate to p2Y; the value is decreased by 50 so the middle of the paddle is stuck to the cursor, rather than the top
-    p2Y = e.clientY - 50;
+    //Saves the mouses Y-coordinate to p2Y; the value is decreased by half of the paddle height so the middle of the paddle is stuck to the cursor, rather than       //the top
+    p2Y = e.clientY - dimX/16;
 }
 
 //Setting the initial position and speed of the ball
 resetBall();
 
 //Calls the update function sixty times per second
-window.setInterval(update, 1000 / 60);
+window.setInterval(update, 50 / 3);
