f2710c983bf1fce51aef548703b1606a25501351
André Vennberg <mac@macs-MacBook-Air.local>
2015-11-24 13:24:58 +0100

    added character, some shading, and some other stuff

--- LektionArray/script.js
+++ LektionArray/script.js
@@ -1,14 +1,30 @@
 var map_canvas = document.getElementById("map");
 var map_ctx = map_canvas.getContext("2d");
 
+var char_canvas = document.getElementById("char");
+var char_ctx = char_canvas.getContext("2d");
+
 var mapHeight = 500;
 var mapWidth = 500;
 
-var scalingX = 1;
-var scalingY = 1;
+var regionSizeX = 10;
+var regionSizeY = 10;
+
+var scalingX = 10;
+var scalingY = 10;
+
+var charX = 500;
+var charY = 200;
+
+var charWidth = 10;
+var charHeight = 10;
+var charStep = 10;
 
 map_ctx.canvas.width = mapHeight * scalingX;
 map_ctx.canvas.height = mapWidth * scalingY;
+
+char_ctx.canvas.width = mapHeight * scalingX;
+char_ctx.canvas.height = mapWidth * scalingY;
 
 var map = [];
 var regionMap = [];
@@ -42,7 +58,7 @@ var spliceMap = function (mapSplice) {
     }
 
     for (var i = 0; i < mapSplice.length; i++) {
-        arr.push(mapSplice[i].splice(0, 10));
+        arr.push(mapSplice[i].splice(0, regionSizeY));
     }
 
     return arr;
@@ -97,7 +113,7 @@ var fillMap = function () {
     window.seedValue = Math.random();
     noise.seed(seedValue);
 
-    var regions = (mapWidth / 10) * (mapHeight / 10);
+    var regions = (mapWidth / regionSizeX) * (mapHeight / regionSizeY);
 
     for (var region = 0; region < regions; region++) {
         window["region_" + region] = [];
@@ -105,9 +121,9 @@ var fillMap = function () {
 
     var regCount = 0;
 
-    for (var regX = 0; regX < (mapWidth / 10); regX++) {
+    for (var regX = 0; regX < (mapWidth / regionSizeX); regX++) {
         regionMap.push([]);
-        for (var regY = 0; regY < (mapHeight / 10); regY++) {
+        for (var regY = 0; regY < (mapHeight / regionSizeY); regY++) {
             regionMap[regX].push(regCount);
             regCount += 1;
         }
@@ -125,10 +141,10 @@ var fillMap = function () {
 
     map = normalize(map, 2, 1);
 
-    while (map.length >= 10) {
-        var split1D = map.splice(0, 10);
+    while (map.length >= regionSizeX) {
+        var split1D = map.splice(0, regionSizeX);
 
-        for (var y = 0; y < mapHeight / 10; y++) {
+        for (var y = 0; y < mapHeight / regionSizeY; y++) {
             window["region_" + regionMap[mapIter][y]] = spliceMap(split1D);
         }
         mapIter += 1;
@@ -152,14 +168,36 @@ var drawRegion = function (region, indexX, indexY) {
         for (var y = 0; y < region[x].length; y++) {
             //map_ctx.fillStyle = float2color(region[x][y]);
 
-            if (region[x][y] < 0.33) {
+            var tileValue = Math.floor(region[x][y] * 100) / 100;
+
+            if (tileValue < 0.11) {
+                map_ctx.fillStyle = "#bb8e68";
+            }
+            else if (tileValue < 0.22) {
                 map_ctx.fillStyle = "#aa7243";
             }
-            else if (region[x][y] < 0.66) {
-                map_ctx.fillStyle = "green";
+            else if (tileValue < 0.33) {
+                map_ctx.fillStyle = "#885b35";
             }
-            else {
-                map_ctx.fillStyle = "blue";
+
+            else if (tileValue < 0.44) {
+                map_ctx.fillStyle = "#329932";
+            }
+            else if (tileValue < 0.55) {
+                map_ctx.fillStyle = "#008000";
+            }
+            else if (tileValue < 0.66) {
+                map_ctx.fillStyle = "#006600";
+            }
+
+            else if (tileValue < 0.77) {
+                map_ctx.fillStyle = "#3232ff";
+            }
+            else if (tileValue < 0.88) {
+                map_ctx.fillStyle = "#0000ff";
+            }
+            else if (tileValue < 1) {
+                map_ctx.fillStyle = "#0000cc";
             }
 
             var cellWidth = Math.ceil(mapWidth * scalingX / regionMap.length / region.length);
@@ -198,9 +236,88 @@ var updateMap = function () {
     }
 }
 
+var drawChar = function (axis, amount) {
+    char_ctx.clearRect(charX - (charWidth + charStep), charY - (charHeight + charStep), charX + (charWidth + charStep), charY + (charHeight + charStep));
+
+    var regionX = Math.floor(charX / charStep / regionSizeX);
+    var regionY = Math.floor(charY / charStep / regionSizeY);
+
+    var activeRegion = regionMap[regionX][regionY];
+    var tileValue = window["region_" + activeRegion][charX / charStep - regionX * regionSizeX][charY / charStep - regionY * regionSizeY];
+
+    tileValue = Math.floor(tileValue * 100) / 100;
+
+    if (tileValue >= 0.66 && typeof axis !== "undefined") {
+        window["char" + axis.toUpperCase()] -= amount;
+        console.log(["char" + axis.toUpperCase(), amount]);
+    }
+    else {
+        if (charX - (window.innerWidth / 2 + scrollOffsets()[0]) >= charStep && scrollOffsets()[0] < (mapWidth * scalingX) - window.innerWidth) {
+            window.scrollBy(charStep, 0);
+        }
+        else if (charX - (window.innerWidth / 2 + scrollOffsets()[0]) <= -charStep && scrollOffsets()[0] !== 0) {
+            window.scrollBy(-charStep, 0);
+        }
+        else if (charY - (window.innerHeight / 2 + scrollOffsets()[1]) >= charStep && scrollOffsets()[1] < (mapHeight * scalingY) - window.innerHeight) {
+            window.scrollBy(0, charStep);
+        }
+        else if (charY - (window.innerHeight / 2 + scrollOffsets()[1]) <= -charStep && scrollOffsets()[1] !== 0) {
+            window.scrollBy(0, -charStep);
+        }
+    }
+
+    char_ctx.fillStyle = "black";
+    char_ctx.fillRect(charX - charWidth, charY - charHeight, charWidth, charHeight);
+}
+
+/*
+var update = function () {
+    drawChar();
+
+    window.requestAnimationFrame(update);
+}
+*/
+
+var keyPressed = function (e) {
+    var key = e.keyCode;
+
+    e.preventDefault();
+
+    switch (key) {
+        case 37:
+            if (charX >= charStep * 2) {
+                charX -= charStep;
+                drawChar("x", -charStep);
+            }
+            break;
+        case 38:
+            if (charY >= charStep * 2) {
+                charY -= charStep;
+                drawChar("y", -charStep);
+            }
+            break;
+        case 39:
+            if (charX <= mapWidth * scalingX - charStep * 2) {
+                charX += charStep;
+                drawChar("x", charStep);
+            }
+            break;
+        case 40:
+            if (charY <= mapHeight * scalingY - charStep * 2) {
+                charY += charStep;
+                drawChar("y", charStep);
+            }
+            break;
+    }
+}
+
 var eventListeners = function () {
     document.addEventListener("scroll", updateMap);
+
+    document.addEventListener("keydown", keyPressed, event);
 }
 
 fillMap();
 eventListeners();
+drawChar();
+//update();
